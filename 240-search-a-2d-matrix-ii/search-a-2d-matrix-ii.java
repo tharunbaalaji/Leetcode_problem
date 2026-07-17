10c5fecdbf71a42b48d6dac5008573b65246bb1e
Tharun Balaaji S <tharunbalaaji@gmail.com>
2026-07-17 12:09:14 +0530

    Time: 10 ms (7.10%) | Memory: 48.4 MB (26.00%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        boolean found=false;
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=0;j<matrix[i].length;j++)
+            {
+                if(matrix[i][j]==target)
+                {
+                    found=true;
+                    break;
+                }
+            }
+            if(found==true)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
